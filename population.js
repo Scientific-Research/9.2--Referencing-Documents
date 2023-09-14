64e71887d66597e2db21de7ca131f153c0edbe24
Maximilian <scientificresearch309@gmail.com>
2023-09-14 12:45:17 +0200

    populate only the name of author and not other details!

--- population.js
+++ population.js
@@ -47,7 +47,7 @@ async function createCourse(name, author) {
 }
 
 async function listCourses() {
-  const courses = await Course.find().select("author name").populate("author")
+  const courses = await Course.find().select("author name").populate("author","name")
   console.log(courses);
 }
 
